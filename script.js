0d810839cd9763a25165f0947a50afbb218f9bcf
sonalishingade68-cpu <sonalishingade68@gmail.com>
2026-01-12 15:09:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function showMessage() {
+    alert("Thank you for contacting me! I will get back to you soon.");
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+
+// Scroll animation
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 300;
+        if (window.scrollY >= sectionTop) {
+            section.style.opacity = "1";
+            section.style.transform = "translateY(0)";
+        }
+    });
+});
